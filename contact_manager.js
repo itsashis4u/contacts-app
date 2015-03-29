a83cc239ff71f60b2ff045c5975a63335b84efc6
Ashish <itsashis4u@gmail.com>
2015-03-29 14:13:42 +0530

    edit feature updated

--- contact_manager.js
+++ contact_manager.js
@@ -1,10 +1,20 @@
 newContact = new Mongo.Collection('newcontact');
+var getdata = false;
 if (Meteor.isClient) {
 Router.route('/', function(){
   this.render('home');
 })
 Router.route('/addContact', function(){
   this.render('addContact');
+   Meteor.startup(function(){
+    var selectContact = localStorage.getItem('selectContact');
+    if(localStorage.getItem('selectContact') != null){
+     // document.querySelector('#name').value = "Hi"
+    } 
+   })
+})
+Router.route('/editContact', function(){
+  this.render('editContact');
 })
 
 
@@ -17,6 +27,12 @@ Template.home.helpers({
   }
 });
 
+Template.editContact.helpers({
+  a: function () {
+    var selectContact = localStorage.getItem('selectContact');
+  return newContact.find(selectContact);
+  }
+});
 Template.addContact.events({
   'click .submit': function (event) {
     event.preventDefault();
@@ -25,26 +41,72 @@ Template.addContact.events({
     var newPhone = document.getElementById('phone').value;
     var newAddr = document.getElementById('address').value;
 
+    if(!newName.trim() || !newEmail.trim() || !newPhone.trim() || !newAddr.trim()){
+      alert("Blank input");
+    }
+    else{
     console.log(newName, newEmail, newPhone, newAddr);
 if(! Meteor.userId()){
       throw Meteor.Error("not-authorized");
     }
-    newContact.insert({
-      name: newName,
-      email_id: newEmail,
-      phone: newPhone,
-      address: newAddr,
-      createdAt: new Date(),
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    }, function(error){
-      if(!error){
-      console.log("Success");
-      window.location.replace('/')}
-    }).distinct('phone',true);
+    //console.log(newContact.find({username: Meteor.user().username}, {phone : {$ne: newPhone} } ).fetch() )
+  
+  newContact.insert({
+        name: newName,
+        email_id: newEmail,
+        phone: newPhone,
+        address: newAddr,
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username
+      }, function(error){
+        if(!error)
+        console.log("Success");
+        // window.location.replace('/')}
+      });  
+
+}
   }
 });
+Template.editContact.events({
+  'click .submit': function (event) {
+    event.preventDefault();
+    var newName = document.getElementById('name').value;
+    var newEmail = document.getElementById('email').value;
+    var newPhone = document.getElementById('phone').value;
+    var newAddr = document.getElementById('address').value;
 
+    if(!newName.trim() || !newEmail.trim() || !newPhone.trim() || !newAddr.trim()){
+      alert("Blank input");
+    }
+    else{
+    console.log(newName, newEmail, newPhone, newAddr);
+if(! Meteor.userId()){
+      throw Meteor.Error("not-authorized");
+    }
+    //console.log(newContact.find({username: Meteor.user().username}, {phone : {$ne: newPhone} } ).fetch() )
+    if(localStorage.getItem('selectContact') != null){
+      var selectContact = localStorage.getItem('selectContact')
+      newContact.update(selectContact,
+        {
+        name: newName,
+        email_id: newEmail,
+        phone: newPhone,
+        address: newAddr,
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username
+      }, function(){
+        console.log("Updated");
+        localStorage.removeItem('selectContact');
+      }
+        );
+      }
+       
+
+}
+  }
+});
 Template.contactList.helpers({
   contactname: function () {
     return newContact.find({username: Meteor.user().username}, {sort:{name:1}});
@@ -61,14 +123,25 @@ Template.contactList.events({
   'click .card': function () {
     var contactId = this._id;
     Session.set('selectContact', contactId);
+    console.log(contactId, newContact.findOne(contactId).name, newContact.findOne(contactId).username);
   },
   'click .remove.user.icon': function(){
     var contactId = this._id;
     Session.set('selectContact', contactId);
-    var selectContact = Session.get('selectContact');
-    var c = confirm(newContact.findOne(selectContact).name + " will be deleted");
+    setTimeout(function(){
+
+    var c = confirm(newContact.findOne(contactId).name + " will be deleted");
     if(c)
-      newContact.remove(selectContact);
+      newContact.remove(contactId);
+    }, 300)
+  },
+  'click .edit': function(event){
+    var contactId = this._id;
+    Session.set('selectContact', contactId);
+    var selectContact = Session.get('selectContact');
+    localStorage.setItem('selectContact', selectContact);
+    // console.log(selectContact)
+    window.open("/editContact", '_self');
   }
 });
 
